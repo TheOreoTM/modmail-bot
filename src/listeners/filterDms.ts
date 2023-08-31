b7a883bae10e89e4269b0cb1fb31e6d7595b1079
TheOreoTM <qwertzybad@gmail.com>
2023-08-31 09:50:00 +0500

    ohh

--- src/listeners/filterDms.ts
+++ src/listeners/filterDms.ts
@@ -3,11 +3,13 @@ import { isDMChannel } from '@sapphire/discord.js-utilities';
 import { Listener } from '@sapphire/framework';
 import { Message } from 'discord.js';
 import { AssistantEvents } from '#constants';
+import { Modmail } from '#lib/classes/Modmail';
 
 @ApplyOptions<Listener.Options>({ event: AssistantEvents.MessageCreate })
 export class UserEvent extends Listener {
-	public override run(message: Message) {
-		if (!isDMChannel(message.channel)) return;
+	public override async run(message: Message) {
+		const isModlogChannel = await new Modmail().isModlogChannel(message.channelId);
+		if (!isDMChannel(message.channel) || !isModlogChannel) return;
 
 		return this.container.client.emit(AssistantEvents.ModmailCreate, message);
 	}
