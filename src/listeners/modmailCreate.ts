0e7504d4bf9b7e49cf7dcdb512aa6587cca08aa6
TheOreoTM <qwertzybad@gmail.com>
2023-09-01 16:59:44 +0500

    rewrite

--- src/listeners/modmailCreate.ts
+++ src/listeners/modmailCreate.ts
@@ -1,75 +1,52 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Listener } from '@sapphire/framework';
-import { AssistantEvents, ModmailDirection } from '#constants';
-import { Message, TextChannel } from 'discord.js';
+import { AssistantEvents, ModmailColors, ModmailDirection } from '#constants';
+import { EmbedBuilder, Message, userMention } from 'discord.js';
 import { Modmail } from '#classes/Modmail';
 import { ModmailTransmission } from '#lib/types';
+import { isDMChannel } from '@sapphire/discord.js-utilities';
 
 @ApplyOptions<Listener.Options>({ event: AssistantEvents.ModmailCreate })
 export class UserEvent extends Listener {
 	public override async run(message: Message) {
-		const modmailManager = new Modmail();
-		const isModlogChannel = await modmailManager.isModlogChannel(message.channelId);
+		const user = message.author;
+		const ModmailManager = new Modmail();
+		let isFirstTime = await ModmailManager.existsFor(user.id);
+		const modmail = await ModmailManager.get({ userId: user.id });
+		if (!modmail) isFirstTime = true;
 
-		const shouldCreateChannel = !isModlogChannel;
-		console.log('🚀 ~ file: modmailCreate.ts:15 ~ UserEvent ~ overriderun ~ shouldCreateChannel:', shouldCreateChannel);
+		if (isFirstTime || !modmail) {
+			const modmail = await ModmailManager.create({ userId: user.id });
+			const channel = await ModmailManager.createChannel({ user: user, modmail: modmail });
+			await ModmailManager.setChannel(modmail.id, channel.id);
 
-		let direction: ModmailDirection = ModmailDirection.ToServer;
-		if (isModlogChannel) {
-			direction = ModmailDirection.ToUser;
+			const baseModmailEmbed = new EmbedBuilder().setTitle('Modmail Started').setColor(ModmailColors.ONGOING);
+
+			const userModmailEmbed = baseModmailEmbed //
+				.setDescription('Your message has been sent. Please wait patiently for a staff member to respond.')
+				.setColor(ModmailColors.Send);
+
+			const serverModmailEmbed = baseModmailEmbed //
+				.setDescription(`New modmail from ${userMention(user.id)}`)
+				.setColor(ModmailColors.Receive);
+
+			user.send({ embeds: [userModmailEmbed] });
+			const msg = await channel.send({ embeds: [serverModmailEmbed] });
+			if (msg.pinnable) msg.pin();
+
+			return;
 		}
 
-		let firstTime = !(await modmailManager.existsFor(message.author.id));
-		console.log('🚀 ~ file: modmailCreate.ts:23 ~ UserEvent ~ overriderun ~ firstTime:', firstTime);
-
-		if (shouldCreateChannel) {
-			const channel = await modmailManager.getChannel((await modmailManager.get({ userId: message.author.id }))!.id).catch(async () => {
-				const channel = await modmailManager.createChannel({ user: message.author, modmail: modmail! });
-				return await modmailManager.setChannel(modmail.id, channel.id);
-			});
-
-			const data: ModmailTransmission = {
-				channel: message.channel as TextChannel,
-				direction: ModmailDirection.ToServer,
-				firstTime: firstTime,
-				modmail: (await modmailManager.get({ channelId: channel.id }))!
-			};
-			return this.container.client.emit(AssistantEvents.ModmailMessageCreate, message, data);
-		}
-
-		if (!firstTime) {
-			const modmail = isModlogChannel
-				? (await modmailManager.get({ channelId: message.channelId }))!
-				: (await modmailManager.get({ userId: message.author.id }))!;
-
-			const channel = await modmailManager.getChannel((await modmailManager.get({ userId: modmail.userId }))!.id).catch(async () => {
-				const channel = await modmailManager.createChannel({ user: message.author, modmail: modmail! });
-				return await modmailManager.setChannel(modmail.id, channel.id);
-			});
-			const data: ModmailTransmission = {
-				channel,
-				direction,
-				firstTime: firstTime,
-				modmail: modmail
-			};
-
-			return this.container.client.emit(AssistantEvents.ModmailSendMessage, message, data);
-		}
-		firstTime = true;
-
-		const modmail = await modmailManager.create({ userId: message.author.id });
-
-		const channel = await modmailManager.createChannel({ user: message.author, modmail: modmail });
-
-		await modmailManager.setChannel(modmail.id, channel.id);
+		const isDM = isDMChannel(message.channel);
+		const modmailChannel = await ModmailManager.getChannel(modmail.id);
 
 		const data: ModmailTransmission = {
-			channel,
-			direction,
-			firstTime: firstTime,
+			channel: modmailChannel,
+			direction: isDM ? ModmailDirection.ToServer : ModmailDirection.ToUser,
+			firstTime: isFirstTime,
 			modmail: modmail
 		};
 
-		return this.container.client.emit(AssistantEvents.ModmailSendMessage, message, data);
+		this.container.client.emit(AssistantEvents.ModmailMessageCreate, message, data);
 	}
 }
